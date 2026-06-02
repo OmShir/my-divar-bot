0c5ea0747891f84452ea50228dd615a50507baca
OmShir <mr.o.shirzad@gmail.com>
2026-06-02 10:32:32 +0330

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,30 +1,49 @@
-def parse_ads(state):
+import re
+
+
+def extract_price(text):
+
+    if not text:
+        return 0
+
+    nums = re.findall(r"\d+", str(text))
+
+    if not nums:
+        return 0
+
+    return int("".join(nums))
+
+
+def parse_ads(data):
 
     ads = []
 
     try:
-        widgets = state.get("widgetList", [])
+
+        widgets = data.get("web_widgets", [])
 
         for w in widgets:
 
-            data = w.get("data")
-
-            if not data:
+            if w.get("@type") != "POST_ROW":
                 continue
 
-            for item in data:
+            d = w.get("data", {})
 
-                try:
-                    ads.append({
-                        "title": item.get("title", ""),
-                        "price": item.get("price", 0),
-                        "url": "https://divar.ir/v/" + item.get("token", "")
-                    })
+            ads.append({
+                "title": d.get("title", ""),
+                "price": extract_price(
+                    d.get("middle_description")
+                ),
+                "url": "https://divar.ir/v/" + d.get(
+                    "action", {}
+                ).get(
+                    "payload", {}
+                ).get(
+                    "web_url", ""
+                )
+            })
 
-                except:
-                    continue
+    except Exception as e:
+        print("PARSE ERROR:", e)
 
-    except:
-        pass
-
-    return ads[:30]
+    return ads
