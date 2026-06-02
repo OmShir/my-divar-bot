0d31cbc135245de8a53d63ec41676eef6ab039a8
OmShir <mr.o.shirzad@gmail.com>
2026-06-02 10:35:46 +0330

    Update collector.py

--- collector.py
+++ collector.py
@@ -1,5 +1,4 @@
 import requests
-from config import LIMIT
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0",
@@ -7,30 +6,32 @@ HEADERS = {
 }
 
 
-URL = "https://api.divar.ir/v8/web-search/"
+URL = "https://api.divar.ir/v8/web-search/tehran/game-console"
 
 
 def fetch_raw():
 
-    payload = {
-        "json_schema": {
-            "category": {
-                "value": "game-console"
-            },
-            "query": "PS5"
-        },
-        "size": LIMIT
-    }
+    try:
 
-    r = requests.post(
-        URL,
-        json=payload,
-        headers=HEADERS,
-        timeout=15
-    )
+        r = requests.get(
+            URL,
+            headers=HEADERS,
+            timeout=15
+        )
 
-    if r.status_code != 200:
-        print("HTTP ERROR:", r.status_code)
+        print("STATUS:", r.status_code)
+
+        if r.status_code != 200:
+            return None
+
+        data = r.json()
+
+        print("RAW KEYS:", data.keys())
+
+        return data
+
+    except Exception as e:
+
+        print("FETCH ERROR:", e)
+
         return None
-
-    return r.json()
